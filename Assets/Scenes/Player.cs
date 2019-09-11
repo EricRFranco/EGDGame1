fa32bfd8b62ef2f113bf621db3fe77538ec5d005
Eric Franco <france2@rpi.edu>
2019-09-11 15:38:20 -0400

    Player moves left and right, can toggle movement on and off

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public bool canMove;
+
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            _rb.velocity = new Vector2(horizontal * speed, _rb.velocity.y);
+        }  
+    }
+}
