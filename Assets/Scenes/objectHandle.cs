16159bf0102a1170a4bad7d0dee5ac0099e88d9a
Sam Chernick <zenkdare@gmail.com>
2019-09-11 15:14:50 -0400

    first code and controller obj

---
+++ Assets/Scenes/objectHandle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectHandle : MonoBehaviour
+{
+    public GameObject control;
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GameObject.Find("Controller");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
