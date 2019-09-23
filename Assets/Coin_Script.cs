905ed4280e5db89356e555c4a5c2d0f0d7744610
Sam Chernick <zenkdare@gmail.com>
2019-09-23 19:21:01 -0400

    coin stuff

---
+++ Assets/Coin_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt(name, 0) == 1)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            PlayerPrefs.SetInt(name, 1);
+            PlayerPrefs.SetInt("num_coins", PlayerPrefs.GetInt("num_coins", 0) + 1);
+            Destroy(gameObject);
+        }
+    }
+}
