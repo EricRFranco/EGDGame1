95049012ec917a41e5339613b79b4b7f5cb7d289
Sam Chernick <zenkdare@gmail.com>
2019-09-24 19:24:30 -0400

    added code for platforms

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -9,6 +9,8 @@ public class MenuScript : MonoBehaviour
     {
         PlayerPrefs.SetInt("map", 0);
         PlayerPrefs.SetInt("hitbox", 0);
+        PlayerPrefs.SetInt("plats", 0);
+        PlayerPrefs.SetInt("plat_box", 0);
     }
     public void Play()
     {
@@ -26,5 +28,13 @@ public class MenuScript : MonoBehaviour
     public void Hitbox()
     {
         PlayerPrefs.SetInt("hitbox", 1);
+    }
+    public void Plats()
+    {
+        PlayerPrefs.SetInt("plats", 1);
+    }
+    public void Plat_Box()
+    {
+        PlayerPrefs.SetInt("plat_box", 1);
     }
 }
